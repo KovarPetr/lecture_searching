f7f6f1ce9857f925e326a9e48ee5a9194ed97e38
Kovář Petr (257014) <257014@vutbr.cz>
2024-04-08 12:56:23 +0200

    Hotový dna hledač

--- searching.py
+++ searching.py
@@ -5,6 +5,20 @@ import json
 cwd_path = os.getcwd()
 
 
+def pattern_search(sequence, pattern):
+    indx = 0
+    final_set = {""}
+    final_set.remove("")
+    for letter in sequence:
+        if indx > len(sequence)-len(pattern):
+            return final_set
+        partial_sequence = sequence[indx:indx + len(pattern)]
+        if pattern == partial_sequence:
+            final_set.add(int(indx - ((len(pattern) + 1) % 2) + (len(pattern) - len(pattern) % 2)/2))
+        indx = indx + 1
+    return final_set
+
+
 def linear_search(searched_list, tested_integer):
     dictionary = {}
     indx = 0
@@ -40,10 +54,13 @@ def read_data(file_name, field):
 
 
 def main():
+    sequence = read_data("sequential.json", "dna_sequence")
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
     search = linear_search(sequential_data, 0)
     print(search)
+    dna = pattern_search(sequence, "ATA")
+    print(dna)
     pass
 
 
